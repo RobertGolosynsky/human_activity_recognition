36a0277781d75027d322c6d64e7abe91cbda5e6f
Vladyslav Tkachuk <tkachuk.v26@gmail.com>
2018-06-25 22:47:37 +0200

    Updates in DataController

--- backend/web-users/src/main/java/com/cra/controller/rest/DataController.java
+++ backend/web-users/src/main/java/com/cra/controller/rest/DataController.java
@@ -29,22 +29,21 @@ public class DataController {
             return ResponseEntity.badRequest().body(new CRAErrorResponse("Recording already exists!"));
         } else {
             user.getRecordings().add(recording);
-            User savedUser = userRepository.save(user);
-
-            List<RecordingDTO> result = savedUser.getRecordings()
-                    .stream()
-                    .map(p -> new RecordingDTO(p.getId(), p.getDate()))
-                    .collect(Collectors.toList());
+            userRepository.save(user);
+            final RecordingDTO result = new RecordingDTO(recording.getId(), recording.getDate());
 
             return ResponseEntity.ok(result);
         }
     }
 
     @GetMapping("/list")
-    public @ResponseBody List<Calendar> getAllData(Principal principal) {
+    public @ResponseBody List<RecordingDTO> getAllData(Principal principal) {
         final User user = userRepository.findByEmail(principal.getName());
-
-        return user.getRecordings().stream().map(p -> p.getDate()).collect(Collectors.toList());
+        List<RecordingDTO> result = user.getRecordings()
+                .stream()
+                .map(p -> new RecordingDTO(p.getId(), p.getDate()))
+                .collect(Collectors.toList());
+        return result;
     }
 
     private static class RecordingDTO {
